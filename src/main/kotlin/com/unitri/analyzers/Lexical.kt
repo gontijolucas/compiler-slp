8233246a7d8b1a9ff3ef7028a20f615544554fd9
Lucas Gontijo <Lucasgontijo0@gmail.com>
2019-03-06 01:41:02 -0300

    Added Lexical Analyzer.

---
+++ src/main/kotlin/com/unitri/analyzers/Lexical.kt
@@ -0,0 +1,57 @@
+package com.unitri.analyzers
+
+import com.unitri.table.Token
+import com.unitri.table.TokenTable
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.regex.Pattern
+
+class Lexical {
+
+    companion object {
+
+        fun createTable(lines: MutableList<String>): TokenTable {
+
+            removeComments(lines)
+            removeExcessSpace(lines)
+
+            val table = TokenTable()
+
+            val index = AtomicInteger(0)
+            lines.forEach {
+                table.addTokens(nextLine(it, index.toInt()))
+                index.addAndGet(1)
+            }
+
+            return table
+        }
+
+        private fun removeComments(lines: MutableList<String>) {
+            lines.removeIf { it.startsWith("#") }
+        }
+
+        private fun removeExcessSpace(lines: MutableList<String>) {
+            lines.removeIf { it.contentEquals("") }
+            lines.replaceAll { it.trim().replace("\\s{2,}".toRegex(), " ") }
+        }
+
+        private fun nextLine(line: String, lineNumber: Int?): MutableList<Token> {
+
+            val tokenTable = TokenTable()
+
+            val symbolSequences = ArrayList<String>()
+            val matcher = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(line)
+            while (matcher.find()) {
+                symbolSequences.add(matcher.group(1)) // Add .replace("\"", "") to remove surrounding quotes.
+            }
+
+            var column = 0
+            for (sequence in symbolSequences) {
+                tokenTable.createTokenAndPutInTable(Token.TokenClass.getClass(sequence), sequence, lineNumber, column)
+                column += sequence.length + 1
+            }
+
+            return tokenTable.tokens
+        }
+    }
+}
